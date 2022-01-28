7b9cb34f275a23b64cb6a84c3b641cfefaa3170d
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-01-28 23:31:45 +0100

    test: add ArrayStackTest tests

--- engine/collection/stack/ArrayStackTest.java
+++ engine/collection/stack/ArrayStackTest.java
@@ -1,5 +1,89 @@
 package engine.collection.stack;
 
-public class ArrayStackTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ArrayStackTest {
+    @Test
+    public void Push_Should_ThrowIllegalArgumentException_When_ElementIsNull() {
+        ArrayStackMock stack = new ArrayStackMock(0);
+        assertThatThrownBy(() -> stack.push(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Push_Should_PushElementOnStack_When_ElementNonNull() {
+        ArrayStackMock stack = new ArrayStackMock(1);
+        Object object = new Object();
+        Object object2 = new Object();
+
+        stack.push(object);
+        stack.push(object2);
+
+        assertThat(stack.size()).isEqualTo(2);
+        assertThat(stack).containsExactly(object, object2);
+    }
+
+    @Test
+    public void PopOrNull_Should_ReturnNull_When_StackIsEmpty() {
+        ArrayStackMock stack = new ArrayStackMock(0);
+        assertThat(stack.popOrNull()).isEqualTo(null);
+    }
+
+    @Test
+    public void PopOrNull_Should_PopElementFromStack() {
+        Object object = new Object();
+        Object object2 = new Object();
+        ArrayStackMock stack = new ArrayStackMock(new Object[]{object, object2}, 2);
+
+        assertThat(stack.popOrNull()).isEqualTo(object2);
+        assertThat(stack.popOrNull()).isEqualTo(object);
+    }
+
+    @Test
+    public void getHeadOrNull_Should_ReturnNull_When_StackIsEmpty() {
+        ArrayStackMock stack = new ArrayStackMock(0);
+        assertThat(stack.getHeadOrNull()).isEqualTo(null);
+    }
+
+    @Test
+    public void getHeadOrNull_Should_ReturnHead() {
+        Object object = new Object();
+        Object object2 = new Object();
+        ArrayStackMock stack = new ArrayStackMock(new Object[]{object, object2}, 2);
+
+        assertThat(stack.getHeadOrNull()).isEqualTo(object2);
+    }
+
+    @Test
+    public void clear_Should_ClearStack() {
+        Object object = new Object();
+        Object object2 = new Object();
+        ArrayStackMock stack = new ArrayStackMock(new Object[]{object, object2}, 2);
+
+        stack.clear();
+
+        assertThat(stack.getArray()).isEmpty();
+    }
+
+    private static class ArrayStackMock extends ArrayStack<Object> {
+        public ArrayStackMock() {
+            this(0);
+        }
+
+        public ArrayStackMock(int defaultCapacity) {
+            super(defaultCapacity);
+        }
+
+        public ArrayStackMock(Object[] array, int size) {
+            this.array = array;
+            this.size = size;
+        }
+
+        public Object[] getArray() {
+            return array;
+        }
+    }
 }
